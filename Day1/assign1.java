41fcbfa234aa1ae69a8dda5d48fdeac58d8c388b
sakshi_kasar <kasarsakshi09@gmail.com>
2024-08-28 09:30:17 +0530

    day1.added

--- Day1/assign1.java
+++ Day1/assign1.java
@@ -1,5 +1,13 @@
 class Demo{
-public static void main (String[] args){
-System.out.println("Hello World");
-}
+ public static void main (String[] args){
+ int a=1;
+ if(a==0){
+   System.out.println("number is zero");
+  }else if(a>0){
+     System.out.println("number is positive");
+   }else{
+     System.out.println("number is negative");
+   }
+
+ }
 }
